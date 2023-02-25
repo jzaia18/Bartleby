6423abbc60f14b583c153f89ba02bbdcdcf764ed
Jake Zaia <jlz2369@g.rit.edu>
2023-02-25 15:01:30 -0500

    OpenAI testing

---
+++ openai/issue_prompt.py
@@ -0,0 +1,28 @@
+import openai
+
+SECRET_KEY_LOCATION = 'secrets.txt'
+
+with open(SECRET_KEY_LOCATION) as f:
+    openai.api_key = f.read().strip()
+
+
+def get_gpt_response(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+    )
+
+    return response
+
+def get_response_text(response):
+    return response['choices'][0]['text']
+
+if __name__ == '__main__':
+    r = get_gpt_response(input())
+
+    print(get_response_text(r))
