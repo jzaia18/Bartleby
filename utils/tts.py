4cf04d7cd67cf82db8f7f8c7cfbeb8dea9ad6aa9
Jake Zaia <jlz2369@g.rit.edu>
2023-02-25 18:01:22 -0500

    Loop together GPT and tts

---
+++ utils/tts.py
@@ -0,0 +1,36 @@
+# Butler Gnome
+# BrickHack 9
+# API caller for eleven labs to make chatgpt into a normal voice.
+
+import requests
+from pygame import mixer
+from time import sleep
+
+API_KEY_LOCATION = 'joerogansecrets.txt'
+with open(API_KEY_LOCATION) as f:
+    API_KEY = f.read().strip()
+
+def get_tts(text, response_loc='response.mp3'):
+    url = 'https://api.elevenlabs.io/v1/text-to-speech/sIA6m5lChGWOhQao1cw5'
+    headers = {'accept': 'audio/mpeg', 'xi-api-key': API_KEY, 'Content-Type': 'application/json'}
+    data = {
+        "text": text,
+        "voice_settings": {"stability": 0.75, "similarity_boost": 0.75}
+    }
+
+    r = requests.post(url,headers=headers, json=data)
+
+    print("Status Code", r.status_code)
+    with open(response_loc, 'wb') as f:
+        f.write(r.content)
+
+def play_tts(fname='response.mp3'):
+    mixer.music.load(fname)
+    mixer.music.play()
+    while mixer.music.get_busy():  # wait for music to finish playing
+        sleep(1)
+
+if __name__ == '__main__':
+    mixer.init()
+    get_tts(input())
+
