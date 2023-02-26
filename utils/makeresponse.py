ce789c2ad2a10111e7b693ef03a9b2132e1c0058
yelsharawy <yusufse-2004@hotmail.com>
2023-02-26 01:52:34 -0500

    energetic forest gnome

--- utils/makeresponse.py
+++ utils/makeresponse.py
@@ -10,7 +10,7 @@ def get_gpt_response(prompt, persona='Joe Rogan'):
     response = openai.Completion.create(
         model="text-curie-001",
         prompt=f"""
-Respond passionately as a gnome named Bartleby: "{prompt}"
+Respond as an energetic forest gnome named Bartleby: "{prompt}"
 """,
         # prompt=f'Answer as if you were {persona}, but your name is Bartleby and you are a gnome: {prompt}',
 #         prompt=f"""
