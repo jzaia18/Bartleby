526c3140aa66368596abe4389efe7905b96cf17c
Jake Zaia <jlz2369@g.rit.edu>
2023-02-25 19:47:53 -0500

    More hotfixes

--- utils/gnome.py
+++ utils/gnome.py
@@ -19,11 +19,13 @@ if __name__ == '__main__':
     print('A:', response)
 
     get_tts(response)
-    play_tts(queue=True)
-
     while mixer.music.get_busy():  # wait for music to finish playing
         sleep(1)
 
+    play_tts()
+    while mixer.music.get_busy():  # wait for music to finish playing
+        sleep(1)
+    
     # mixer.music.load('response.mp3')
     # mixer.music.play()
     # while mixer.music.get_busy():  # wait for music to finish playing
